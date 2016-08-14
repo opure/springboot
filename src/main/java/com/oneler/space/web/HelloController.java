7785c96df2de8456726a6dc6f9540bc1b1c693a4
opure <myoneler@hotmail.com>
2016-08-15 00:34:51 +0800

    springboot test

--- src/main/java/com/oneler/space/web/HelloController.java
+++ src/main/java/com/oneler/space/web/HelloController.java
@@ -27,6 +27,6 @@ public class HelloController {
         // 加入一个属性，用来在模板中读取
         map.addAttribute("host", "http://blog.didispace.com");
         // return模板文件的名称，对应src/main/resources/templates/index.html
-        return "a";
+        return "index";
     }
 }
